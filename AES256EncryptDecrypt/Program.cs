f78727fefeca4f31efd047e1236672620ebc9e6a
cg10036 <30382481+cg10036@users.noreply.github.com>
2019-06-16 00:32:03 +0900

    Add files via upload

---
+++ AES256EncryptDecrypt/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace AES256EncryptDecrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+                return;
+            string passwd, md5_passwd;
+            string encoded = Convert.ToBase64String(File.ReadAllBytes(args[0])); //Encrypt
+            //string encoded = File.ReadAllText(args[0]); //Decrypt
+            passwd = Console.ReadLine();
+            md5_passwd = MD5Hash(passwd);
+            File.WriteAllText(args[0] + ".AES256", AESEncrypt256(encoded, md5_passwd)); //Encrypt
+            //string decrypt = Decrypt256(encoded, md5_passwd); //Decrypt
+            //File.WriteAllBytes(Path.GetFileNameWithoutExtension(args[0]), Convert.FromBase64String(decrypt)); //Decrypt
+        }
+
+        static string MD5Hash(string str)
+        {
+            StringBuilder MD5Str = new StringBuilder();
+            byte[] byteArr = Encoding.ASCII.GetBytes(str);
+            byte[] resultArr = (new MD5CryptoServiceProvider()).ComputeHash(byteArr);
+            for(int cnti = 0;cnti < resultArr.Length;cnti++)
+            {
+                MD5Str.Append(resultArr[cnti].ToString("X2"));
+            }
+            return MD5Str.ToString();
+        }
+
+        static string Decrypt256(String Input, String key)
+        {
+            RijndaelManaged aes = new RijndaelManaged();
+            aes.KeySize = 256;
+            aes.BlockSize = 128;
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+            aes.Key = Encoding.UTF8.GetBytes(key);
+            aes.IV = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            var decrypt = aes.CreateDecryptor();
+            byte[] xBuff = null;
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(ms, decrypt, CryptoStreamMode.Write))
+                {
+                    byte[] xXml = Convert.FromBase64String(Input);
+                    cs.Write(xXml, 0, xXml.Length);
+                }
+
+                xBuff = ms.ToArray();
+            }
+
+            String Output = Encoding.UTF8.GetString(xBuff);
+            return Output;
+        }
+
+        static String AESEncrypt256(String Input, String key)
+        {
+            RijndaelManaged aes = new RijndaelManaged();
+            aes.KeySize = 256;
+            aes.BlockSize = 128;
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+            aes.Key = Encoding.UTF8.GetBytes(key);
+            aes.IV = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            var encrypt = aes.CreateEncryptor(aes.Key, aes.IV);
+            byte[] xBuff = null;
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(ms, encrypt, CryptoStreamMode.Write))
+                {
+                    byte[] xXml = Encoding.UTF8.GetBytes(Input);
+                    cs.Write(xXml, 0, xXml.Length);
+                }
+
+                xBuff = ms.ToArray();
+            }
+
+            String Output = Convert.ToBase64String(xBuff);
+            return Output;
+        }
+    }
+}
